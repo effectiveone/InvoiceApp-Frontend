0fe03e4875a4f9cea44daaa9489745334a3cea63
Konrad <konrad.gruca90@gmail.com>
2023-03-15 16:03:57 +0100

    added translation to a project

--- src/Pages/Settings.js
+++ src/Pages/Settings.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../shared/components/layout/layout";
 import { useSelector, useDispatch } from "react-redux";
 import { setTheme, changeTheme } from "../store/actions/designActions";
+import { useTranslation } from "react-i18next";
+import { US, PL, FR } from "country-flag-icons/react/3x2";
+import { v4 as uuid } from "uuid";
+import i18n from "../i18n";
 
 const Settings = () => {
+  const [language, setLanguage] = useState("en");
+  const options = [
+    { value: "en", icon: <US /> },
+    { value: "pl", icon: <PL /> },
+    { value: "fr", icon: <FR /> },
+  ];
+
+  useEffect(() => {
+    i18n.changeLanguage(language);
+  }, [language, setLanguage]);
   const mySystemOfDesign = useSelector(
     (state) => state.design.mySystemOfDesign
   );
@@ -21,14 +35,64 @@ const Settings = () => {
     dispatch(setTheme(selectedDesign));
   };
   return (
-    <select value={selectedDesign?.name} onChange={handleThemeChange}>
-      {mySystemOfDesign?.map((design) => (
-        <option key={design.name} value={design.name}>
-          {design.name}
-        </option>
-      ))}
-    </select>
+    <>
+      <select value={selectedDesign?.name} onChange={handleThemeChange}>
+        {mySystemOfDesign?.map((design) => (
+          <option key={design.name} value={design.name}>
+            {design.name}
+          </option>
+        ))}
+      </select>
+      <CustomSelect
+        value={language}
+        onChange={(e) => setLanguage(e)}
+        options={options}
+        className="language-select"
+        width="30px"
+      />
+    </>
   );
 };
 
 export default Layout(Settings);
+
+const CustomSelect = ({ value, onChange, options, className, width }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState({
+    value: "en",
+    icon: <US />,
+  });
+
+  const toggleOptions = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const selectOption = (option) => {
+    setSelectedOption(option);
+    onChange(option.value);
+    toggleOptions();
+  };
+
+  return (
+    <div
+      className={`select-container ${className}`}
+      style={{ width }}
+      onClick={toggleOptions}
+    >
+      <div className="selected-option"> {selectedOption.icon}</div>
+      {isOpen && (
+        <div className="options">
+          {options.map((option) => (
+            <div
+              key={uuid()}
+              className="option"
+              onClick={() => selectOption(option)}
+            >
+              {option.icon}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
