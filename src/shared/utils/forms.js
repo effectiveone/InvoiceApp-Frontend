dec12eff70639e6a11104360fb6a487854239311
Lukasz <lukaszgruca92@gmail.com>
2023-04-02 13:09:33 +0200

    added legal form in Poland, #Ticket-002

--- src/shared/utils/forms.js
+++ src/shared/utils/forms.js
@@ -1,6 +1,13 @@
 export const legalForms = [
   { value: "sp.z.o.o", label: "Sp. z o.o." },
-  { value: "sa", label: "SA" },
-  { value: "s.c.", label: "S.C." },
+  { value: "SA", label: "SA" },
+  { value: "S.C.", label: "S.C." },
+  { value: "j. g.", label: "j.g" },
+  { value: "stowarzyszenie", label: "stowarzyszenie" },
+  { value: "spółdzilenie", label: "spółdzilenie" },
+  { value: "fundacje", label: "fundacje" },
+  { value: "s.k.", label: "s.k." },
+  { value: "s.j.", label: "s.j." },
+  { value: "s.k-a.", label: "s.k-a." },
   { value: "inne", label: "Inne" },
 ];
